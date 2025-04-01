cefc1b463b41a3a2aae8fe364a67bc27fea03598
Albert Labarento <albertlabarento@gmail.com>
2025-04-01 16:06:08 +0800

    [finalize_ui_integration] Done - Delete

--- assets/vue/modules/service_log/pages/Dashboard.vue
+++ assets/vue/modules/service_log/pages/Dashboard.vue
@@ -5,41 +5,11 @@ import useFetchCount from "../composables/useFetchCount";
 import ServiceLogFilter from "../components/ServiceLogFilter.vue";
 import useFetchServiceLogs from "../composables/useFetchServiceLogs";
 import moment from "moment-timezone";
+import ServiceLogTable from "../components/ServiceLogTable.vue";
 
 const dateFormat = 'DD/MMM/YYYY HH:mm:ss z';
 
 const table = reactive({
-    isLoading: false,
-    columns: [
-        {
-            label: "Service Name",
-            field: "service_name",
-        },
-        {
-            label: "Date",
-            field: 'log_date',
-        },
-        {
-            label: "Url",
-            field: "url",
-        },
-        {
-            label: "Status Code",
-            field: "status_code",
-        },
-        {
-            label: "Http Verb",
-            field: "http_verb",
-        },
-        {
-            label: "Http Version",
-            field: "http_version",
-        },
-        {
-            label: "Action",
-            field: "action",
-        },
-    ],
     pagination: {
         page: 1,
         pages: 0,
@@ -47,8 +17,8 @@ const table = reactive({
     filters: {},
 });
 
-const {isLoadingCount, run: runFetchCount, data: countData} = useFetchCount();
-const {isLoadingLogs, run: runFetchLogs, data: serviceLogs} = useFetchServiceLogs();
+const {isLoading: isLoadingCount, run: runFetchCount, data: countData} = useFetchCount();
+const {isLoading: isLoadingLogs, run: runFetchLogs, data: serviceLogs} = useFetchServiceLogs();
 
 const query = computed(() => {
     if (!table.filters.serviceNames) {
@@ -72,37 +42,29 @@ const fetchDashboardData = () => {
     runFetchCount(query.value);
     fetchPaginatedLogs();
 }
+
+const onFilter = () => {
+    table.pagination.page = 1;
+    fetchDashboardData();
+}
 </script>
 
 <template>
     <div class="container main centered">
-        <ServiceLogFilter @on-filter="fetchDashboardData" v-model:filters="table.filters"></ServiceLogFilter>
+        <ServiceLogFilter @on-filter="onFilter" v-model:filters="table.filters"></ServiceLogFilter>
 
         <App.Dashboard.CounterWidget
             title="Total logs"
             :count="countData?.count ?? 0"
         ></App.Dashboard.CounterWidget>
 
-        <div>
-            <App.Table.List
-                :columns="table.columns"
-                :data="serviceLogs?.items ?? []"
-            >
-                <template #log_date="{log_date}">
-                    {{ moment.tz(log_date.date, log_date.timezone).format(dateFormat) }}
-                </template>
-
-                <template #action="{log_date}">
-                    <App.Button.Default>Delete</App.Button.Default>
-                </template>
-            </App.Table.List>
-
-            <App.Table.Pagination
-                v-model:page="table.pagination.page"
-                :pages="table.pagination.pages"
-                @on-paginate="fetchDashboardData"
-            />
-        </div>
+        <ServiceLogTable
+            v-model:page="table.pagination.page"
+            :pages="table.pagination.pages"
+            :data="serviceLogs?.items ?? []"
+            @on-paginate="fetchPaginatedLogs"
+            @on-delete="fetchDashboardData"
+        />
     </div>
 </template>
 
